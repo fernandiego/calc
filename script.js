7235db47ef54cb1af254175c73b88ca0a8c4c502
fernandiego <fernandodiego.sn@gmail.com>
2022-05-14 23:13:22 -0300

    mermim um parto

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
+const numberButtons = document.querySelector('[data-number]')
+const operationButtons = document.querySelector('[data-operator]')
+const resultButton = document.querySelector('[data-result]')
+const clearButton = document.querySelector('[data-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const dataPreviousTextElement = document.querySelector('[data-previous-operand]')
+const dataCurrentTextElement = document.querySelector('[data-current-operand]')
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Digite um número para somar\n', (num) => {
-    if (isNaN(num))
-        console.log(`${num} não é um número válido`);
-    else {
-        var b = num
-        rl.question('Digite um número para somar\n', (numb) => {
-            if (isNaN(numb))
-                console.log(`${numb} não é um número válido`);
-            else
-            var a = numb
-            var res = b+a
-                console.log(`resultado da soma: ${res}`);
-            rl.close();
-        })
+class Calculator {
+    constructor(dataPreviousTextElement, dataCurrentTextElement) {
+        this.dataPreviousTextElement = dataPreviousTextElement;
+        this.dataCurrentTextElement = dataCurrentTextElement;
     }
-});
+    clear() {
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
+    updateDisplay () {
+        this.previousOperand
+    }
+}
+const calculator = new Calculator(dataPreviousButton, dataCurrentButton);
 
-rl.question('Digite um número\n', (num) => {
-    if (isNaN(num))
-        console.log(`${num} não é um número válido`);
-    else
-        console.log(`Você digitou o número ${num}`);
-    rl.close();
-});
+clearButton.addEventListener('click', () => {
+    this.clear();
+})
