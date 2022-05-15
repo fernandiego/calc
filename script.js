076fe781a12e8d6875da7a6d1167b7eb1c8e706d
fernandiego <fernandodiego.sn@gmail.com>
2022-05-15 19:44:58 -0300

    finito. Consigo ler, mas fiz com a ajuda do tutori

--- script.js
+++ script.js
@@ -10,22 +10,96 @@ class Calculator {
     constructor(dataPreviousTextElement, dataCurrentTextElement) {
         this.dataPreviousTextElement = dataPreviousTextElement;
         this.dataCurrentTextElement = dataCurrentTextElement;
+        this.clear()
     }
 
-appendNumber(number){
-    this.currentOperand = `${this.currentOperand}${number.toString()}`
-}
+    formatDisplayNumber(number) {
+        const stringNumber = number.toString();
+
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+
+        let integerDisplay;
+
+        if (isNaN(integerDigits)) {
+            integerDisplay = "";
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0,
+            });
+        }
+
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+
+    calculate() {
+        let result;
+
+        const _previousOperand = parseFloat(this.previousOperand);
+        const _currentOperand = parseFloat(this.currentOperand);
+
+        if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+        switch (this.operation) {
+            case "+":
+                result = _previousOperand + _currentOperand;
+                break;
+            case "-":
+                result = _previousOperand - _currentOperand;
+                break;
+            case "÷":
+                result = _previousOperand / _currentOperand;
+                break;
+            case "*":
+                result = _previousOperand * _currentOperand;
+                break;
+            default:
+                return;
+        }
+
+        this.currentOperand = result;
+        this.operation = undefined;
+        this.previousOperand = "";
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === "") return;
+
+        if (this.previousOperand !== "") {
+            this.calculate();
+        }
+
+        this.operation = operation;
+
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
+
+    appendNumber(number) {
+        this.currentOperand = `${this.currentOperand}${number.toString()}`
+    }
 
     clear() {
         this.currentOperand = "";
         this.previousOperand = "";
         this.operation = undefined;
     }
-    updateDisplay () {
+    updateDisplay() {
         this.dataPreviousTextElement.innerText = this.previousOperand;
         this.dataCurrentTextElement.innerText = this.currentOperand;
     }
 }
+
+
 const calculator = new Calculator(dataPreviousTextElement, dataCurrentTextElement);
 
 for (const numberButton of numberButtons) {
@@ -35,7 +109,24 @@ for (const numberButton of numberButtons) {
     })
 }
 
+for (const operationButton of operationButtons) {
+    operationButton.addEventListener("click", () => {
+        calculator.chooseOperation(operationButton.innerText);
+        calculator.updateDisplay();
+    });
+}
+
 clearButton.addEventListener('click', () => {
     calculator.clear();
     calculator.updateDisplay();
-})
+});
+
+resultButton.addEventListener("click", () => {
+    calculator.calculate();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
