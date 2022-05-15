b750dec5a7e17050ecca7107e4d5fa0dfb4442cd
fernandiego <fernandodiego.sn@gmail.com>
2022-05-15 19:48:42 -0300

    corrigindo divisão

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ class Calculator {
             case "-":
                 result = _previousOperand - _currentOperand;
                 break;
-            case "÷":
+            case "/":
                 result = _previousOperand / _currentOperand;
                 break;
             case "*":
